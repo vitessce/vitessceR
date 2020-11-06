6ea6e0b72e88c3535a61b5d9db4a5f2b121a4365
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-11-05 19:10:20 -0500

    Update
    Former-commit-id: ae0343fa9a4e5b75bab4a815dba134b7d44f6514

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
-mkdir -p tests-data
+mkdir -p inst/testdata
 
 # Download the test AnnData file
-curl -L -o tests-data/pbmc3k_filtered_gene_bc_matrices.tar.gz https://cf.10xgenomics.com/samples/cell/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz
-tar -xvzf tests-data/pbmc3k_filtered_gene_bc_matrices.tar.gz -C tests-data
+curl -L -o inst/testdata/pbmc3k_filtered_gene_bc_matrices.tar.gz https://cf.10xgenomics.com/samples/cell/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz
+tar -xvzf inst/testdata/pbmc3k_filtered_gene_bc_matrices.tar.gz -C inst/testdata
--- tests/testthat/test-wrappers-seurat.R
+++ tests/testthat/test-wrappers-seurat.R
@@ -2,7 +2,7 @@ library(vitessce)
 library(Seurat)
 
 test_that("SeuratWrapper create_cells_list", {
-  pbmc.data <- Read10X(data.dir = "../../tests-data/filtered_gene_bc_matrices/hg19")
+  pbmc.data <- Read10X(data.dir = "../../inst/testdata/filtered_gene_bc_matrices/hg19")
 
   pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
   pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
