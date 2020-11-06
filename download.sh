af4bec89ea71e087fe86a575637596e9ee4f475e
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-11-06 18:53:50 -0500

    Fix travis

--- download.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p inst/testdata
-
-# Download the test AnnData file
-curl -L -o inst/testdata/pbmc3k_filtered_gene_bc_matrices.tar.gz https://cf.10xgenomics.com/samples/cell/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz
-tar -xvzf inst/testdata/pbmc3k_filtered_gene_bc_matrices.tar.gz -C inst/testdata
