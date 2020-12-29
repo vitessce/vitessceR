5d1a97fd5427c9c11c2e71f7a192a6ad8d6bded9
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-12-28 23:21:58 -0500

    Fix tests
    Former-commit-id: 4e8852a5f53809822d9e8647ed49f3425dff2b9f

--- tests/testthat/setup-wrappers-seurat.R
+++ tests/testthat/setup-wrappers-seurat.R
@@ -1,6 +1,6 @@
 setup({
   url <- "https://cf.10xgenomics.com/samples/cell/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz"
-  dir.create("seurat")
+  dir.create("seurat", showWarnings = FALSE)
   download.file(url, destfile = "seurat/filtered_gene_bc_matrices.tar.gz")
   untar("seurat/filtered_gene_bc_matrices.tar.gz", exdir = "seurat")
 })
