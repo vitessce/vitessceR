dfbb0222a2e0794604080b93840ba059ec2f86e4
mkeller <7525285+keller-mark@users.noreply.github.com>
2021-04-06 19:19:47 -0400

    Remove extra js

--- inst/htmlwidgets/lib/index.js
+++ inst/htmlwidgets/lib/index.js
@@ -4,9 +4,6 @@ import { Vitessce } from 'vitessce';
 import 'vitessce/dist/es/production/static/css/index.css';
 import './index.css';
 
-var globalThis = window;
-console.log(globalThis);
-
 export function render_vitessce(el, config, width, height, theme) {
     ReactDOM.render(
         <Vitessce
