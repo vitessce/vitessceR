6b48ec0b0ce045e37cf80ffd99572e476cff820a
keller-mark <keller-mark@users.noreply.github.com>
2021-12-01 16:58:48 +0000

    Deployed using Blazing fast GitHub Pages deploy
    Former-commit-id: f09611e8980a4ca4f925fc01457d48c33ab3ce25

---
+++ articles/json_remote_files/vitessce-binding-0.0.0.9007/vitessce.js
@@ -0,0 +1,16 @@
+HTMLWidgets.widget({
+  name: 'vitessce',
+  type: 'output',
+  factory: function(el, width, height) {
+    // TODO: define shared variables for this instance
+
+    return {
+      renderValue: function(x) {
+        render_vitessce(el, x.config, width, height, x.theme);
+      },
+      resize: function(width, height) {
+        // TODO: code to re-render the widget with a new size
+      }
+    };
+  }
+});
