57bfb6246fd62afa17e74bbefa11eb19cef3f310
mkeller <7525285+keller-mark@users.noreply.github.com>
2021-12-04 17:18:00 -0500

    Add spatialexperiment functions
    Former-commit-id: 683f103932e32af6c3f2b54fb3fdf74e0b1d2205

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,14 @@
 # Reference: https://rstudio.github.io/reticulate/articles/package.html
 # global reference to zarr (will be initialized in .onLoad)
 zarr <- NULL
+numpy <- NULL
+anndata <- NULL
+ome_zarr <- NULL
 
 .onLoad <- function(libname, pkgname) {
   # use superassignment to update global reference to zarr
   zarr <<- reticulate::import("zarr", delay_load = TRUE)
+  numpy <<- reticulate::import("numpy", delay_load = TRUE)
+  anndata <<- reticulate::import("anndata", delay_load = TRUE)
+  ome_zarr <<- reticulate::import("ome_zarr", delay_load = TRUE)
 }
