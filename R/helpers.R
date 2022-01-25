0bbc0b885e4827a0dc4537d1d42025b946d2e875
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-01-25 08:55:05 -0500

    Split JS into chunks

--- R/helpers.R
+++ R/helpers.R
@@ -79,12 +79,63 @@ is_na <- function(val) {
 #' @keywords internal
 #' @param f The future to stop
 #' @return Nothing
-stop_future <- function(f){
+stop_future <- function(f) {
   # Reference: https://github.com/HenrikBengtsson/future/issues/93#issuecomment-349625087
   if(!is.null(f$job) && Sys.getpid() != f$job$pid) {
     tools::pskill(f$job$pid, signal = tools::SIGTERM)
     tools::pskill(f$job$pid, signal = tools::SIGKILL)
   }
 }
+
+#' Merge file chunks
+#'
+#' @keywords internal
+#' @param chunk_paths An ordered list of paths to each chunk to be merged.
+#' @param out_path Where to store the merged file.
+#' @return Nothing
+merge_files <- function(chunk_paths, out_path) {
+  binary_chunks <- lapply(chunk_paths, function(chunk_path) {
+    fp <- chunk_path
+    fp_size <- file.info(fp)$size
+    fp_pointer <- file(fp, "rb")
+    fp_data <- readBin(fp_pointer, what = "raw", n = fp_size)
+    close(fp_pointer)
+    return(fp_data)
+  })
+
+  fp <- out_path
+  fp_pointer <- file(fp, "wb")
+  for(binary_chunk in binary_chunks) {
+    writeBin(binary_chunk, fp_pointer)
+  }
+  close(fp_pointer)
+}
+
+#' Merge JS file chunks
+#'
+#' @keywords internal
+#' @param force Should the merging be done even if the merged file already exists? By default, FALSE.
+#' @return Nothing
+merge_js <- function(force = FALSE) {
+  pkg_name <- "vitessceR"
+  dist_dir <- system.file(
+    "inst", "htmlwidgets", "dist",
+    package = pkg_name
+  )
+  js_bundle <- file.path(dist_dir, "index.js")
+  map_bundle <- file.path(dist_dir, "index.js.map")
+  merged_paths <- c(js_bundle, map_bundle)
+  if(!force && all(file.exists(merged_paths))) {
+    return(TRUE)
+  }
+  dist_files <- list.files(dist_dir)
+  js_files <- file.path(dist_dir, dist_files[startsWith(dist_files, "index.js.chunk.")])
+  map_files <- file.path(dist_dir, dist_files[startsWith(dist_files, "index.js.map.chunk.")])
+
+  merge_files(js_files, js_bundle)
+  merge_files(map_files, map_bundle)
+}
+
+
 
 
