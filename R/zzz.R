547697ce678ce3d893c6bbc727fa63fd3453bd1d
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-02-08 15:00:55 -0500

    Try catch in .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,13 @@
 .onLoad <- function(libname, pkgname) {
-    merge_js()
+  tryCatch(
+    expr = {
+      merge_js()
+    },
+    error = function(e){ 
+      message("Error when running merge_js from .onLoad")
+    },
+    warning = function(w){
+      message("Warning when running merge_js from .onLoad")
+    }
+  )
 }
