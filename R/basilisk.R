62092c5ffb1eb94006ad4642b2cdbb3ad85fdddf
mkeller <7525285+keller-mark@users.noreply.github.com>
2023-01-06 12:08:23 -0500

    Relax versions

--- R/basilisk.R
+++ R/basilisk.R
@@ -10,17 +10,17 @@ py_env <- basilisk::BasiliskEnvironment(
   envname="vitessce_basilisk_env",
   pkgname="vitessceR",
   packages=c(
-    "numpy==1.20.2",
-    "pandas==1.2.4",
-    "anndata==0.7.6",
-    "h5py==3.2.1",
-    "hdf5==1.10.6",
-    "natsort==7.1.1",
-    "packaging==20.9",
-    "scipy==1.6.3",
-    "sqlite==3.35.5",
-    "zarr==2.5.0",
-    "numcodecs==0.7.2"
+    "numpy==1.*",
+    "pandas==1.*",
+    "anndata>=0.7.6,<0.9",
+    "h5py==3.*",
+    "hdf5==1.*",
+    "natsort==7.*",
+    "packaging==20.*",
+    "scipy==1.*",
+    "sqlite==3.*",
+    "zarr==2.*",
+    "numcodecs>=0.7.2,<0.12"
   ),
   pip=c(
     "ome-zarr==0.2.1"
