2144ac4cd8909e2c6a9acd0d349b48c01d32b4c7
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2023-09-24 19:07:25 -0400

    OmeTiffWrapper update

--- R/wrappers.R
+++ R/wrappers.R
@@ -1,7 +1,7 @@
 
 
 make_unique_filename <- function(file_ext) {
-  return(paste0("some_uuid", file_ext))
+  return(paste0(uuid::UUIDgenerate(output = "string"), file_ext))
 }
 
 
--- tests/testthat/test-wrappers.R
+++ tests/testthat/test-wrappers.R
@@ -46,3 +46,28 @@ test_that("file_path_to_url_path", {
   # posix_with_dot_without_slash <- file_path_to_url_path("./tests/data/test.snap.mtx", prepend_slash = FALSE)
   # expect_equal(posix_with_dot_without_slash, "tests/data/test.snap.mtx")
 })
+
+test_that("OmeTiffWrapper", {
+  w <- OmeTiffWrapper$new(img_path = "tests/data/test.ome.tiff", name = "Test")
+  w$local_img_uid <- "test.ome.tiff"
+
+  file_def_creator <- w$make_raster_file_def_creator("A", "0")
+  file_def <- file_def_creator("http://localhost:8000")
+
+  expect_equal(file_def, list(
+    fileType = "raster.json",
+    options = list(
+      schemaVersion = "0.0.2",
+      images = list(
+        obj_list(
+          name = "Test",
+          type = "ome-tiff",
+          url = "http://localhost:8000/A/0/test.ome.tiff",
+          metadata = list(
+            isBitmask = FALSE
+          )
+        )
+      )
+    )
+  ))
+})
